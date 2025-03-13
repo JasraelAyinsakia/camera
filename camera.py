34cee5ac243773a18708d35332f786ccf034991f
Jacob Ayinsakia <jacob.ayinsakia@hubtel.com>
2025-03-13 00:08:05 +0000

    Code for camera

---
+++ camera.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from PIL import Image
+
+with st.expander("Start Camera"):
+    #start the camera
+    camera_image = st.camera_input("Camera")
+
+
+if camera_image:
+    #create pillow image intance
+    img = Image.open(camera_image)
+
+    #Convert the pillow image to grayscale
+    gray_image = img.convert("L")
+
+    # Render the grayscale image on the webpage
+    st.image(gray_image)
